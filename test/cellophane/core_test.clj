4b1a518e6611ffee64b7bb3e0a674159197935c1
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-03 23:46:03 +0100

    add tests for defui statics

--- test/cellophane/core_test.clj
+++ test/cellophane/core_test.clj
@@ -10,9 +10,20 @@
   (render [this]
     (dom/div nil "Hello World")))
 
+(defui ComponentWithStatics
+  static om/Ident
+  (ident [this props]
+    [:by-id 42])
+  static om/IQuery
+  (query [this]
+    [:foo]))
+
 (deftest test-defui
   (testing "defui definition works"
     (is SimpleComponent))
   (testing "defui implements Lifecycle protocols"
-    (is (= (.initLocalState SimpleComponent) {:foo 1}))))
+    (is (= (.initLocalState SimpleComponent) {:foo 1})))
+  (testing "defui implements statics"
+    (is (= (.query ComponentWithStatics) [:foo]))
+    (is (= (.ident ComponentWithStatics {}) [:by-id 42]))))
 
