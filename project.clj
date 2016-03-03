5aaba9e17b3c1b3b12114c71d2851867b77657ed
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-03 23:27:36 +0100

    project structure, initial defui impl

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject com.ladderlife/cellophane "0.1.0-SNAPSHOT"
+  :description "Server-side rendering for Om Next components"
+  :url "http://github.com/ladderlife/cellophane"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["clojars" {:sign-releases false}]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 [org.omcljs/om "1.0.0-alpha30" :scope "test"]]
+
+  :jvm-opts ^:replace ["-Xmx1g" "-server"]
+  :source-paths ["src" "test"]
+  :test-paths ["test"]
+  :clean-targets ["target"]
+  :target-path "target"
+
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.7.228"]
+                                  [cljsjs/react-dom-server "0.14.3-0"]]}})
---
+++ test/cellophane/core_test.clj
@@ -0,0 +1,17 @@
+(ns cellophane.core-test
+  (require [clojure.test :refer [deftest testing is are]]
+           [cellophane.next :as om :refer [defui]]))
+
+(defui SimpleComponent
+  Object
+  (initLocalState [this]
+    {:foo 1})
+  (render [this]
+    (dom/div nil "Hello World")))
+
+(deftest test-defui
+  (testing "defui definition works"
+    (is SimpleComponent))
+  (testing "defui implements Lifecycle protocols"
+    (is (= (.initLocalState SimpleComponent) {:foo 1}))))
+
