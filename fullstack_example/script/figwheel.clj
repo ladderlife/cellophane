18f395840b74448c821eb494081406a752ac7e47
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-16 00:25:49 +0000

    add full-stack todomvc example with server-rendering

---
+++ fullstack_example/script/figwheel.clj
@@ -0,0 +1,31 @@
+(require '[figwheel-sidecar.repl :as r]
+         '[figwheel-sidecar.repl-api :as ra])
+(comment
+  (b/build "src/cljs"
+    {:output-to          "resources/public/js/app.js"
+     :output-dir         "resources/public/js"
+     :optimizations      :simple
+     :static-fns         true
+     :optimize-constants true
+     :pretty-print       true
+     :externs            ["src/js/externs.js"]
+     :closure-defines    '{goog.DEBUG false}
+     :verbose            true})
+
+  )
+
+(ra/start-figwheel!
+  {:figwheel-options {}
+   :build-ids ["dev"]
+   :all-builds
+   [{:id "dev"
+     :figwheel true
+     :source-paths ["src/shared"]
+     :compiler {:main 'todomvc.todomvc
+                :asset-path "/js"
+                :output-to "resources/public/js/app.js"
+                :output-dir "resources/public/js"
+                :parallel-build true
+                :compiler-stats true}}]})
+
+(ra/cljs-repl)
