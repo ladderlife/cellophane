66e99da706214017dbd990e3f7dfe35720a3dc76
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-26 23:49:45 +0100

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ladderlife/cellophane "0.2.2"
+(defproject com.ladderlife/cellophane "0.2.3-SNAPSHOT"
   :description "Server-side rendering for Om Next components"
   :url "http://github.com/ladderlife/cellophane"
   :license {:name "Eclipse Public License"
