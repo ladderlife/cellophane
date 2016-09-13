c98b0a95a42d1a92843448f0d1c6aa5439ee8ee9
António Nuno Monteiro <anmonteiro@gmail.com>
2016-09-13 13:24:52 +0200

    bump cljs and clj

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["clojars" {:sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha11" :scope "provided"]
-                 [org.omcljs/om "1.0.0-alpha42" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12" :scope "provided"]
+                 [org.omcljs/om "1.0.0-alpha44" :scope "provided"]]
 
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :source-paths ["src" "test"]
